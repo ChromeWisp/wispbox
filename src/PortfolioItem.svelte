c078f081991a4ec32ef3892741097176f69df3b9
auroratide <tf.auroratide@gmail.com>
2019-11-12 06:52:13 -0600

    Style the skills list

--- src/PortfolioItem.svelte
+++ src/PortfolioItem.svelte
@@ -35,6 +35,8 @@
 
   .portfolio-item section {
     padding: 1em 0;
+    display: flex;
+    flex-direction: column;
   }
 
   .portfolio-item :global(h1) {
@@ -51,19 +53,49 @@
     display: block;
   }
 
+  .portfolio-item :global(.skills-list) {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    font-size: 0.875em;
+    color: var(--color-teal);
+    text-align: right;
+    font-style: italic;
+    flex: 1;
+  }
+
+  .portfolio-item :global(.skills-list li) {
+    display: inline-block;
+  }
+
+  .portfolio-item :global(.skills-list li::after) {
+    content: ',';
+    margin-right: 0.125em;
+  }
+
+  .portfolio-item :global(.skills-list li:last-child::after) {
+    content: '';
+    margin-right: 0;
+  }
+
   @media (min-width: 1024px) {
     .portfolio-item {
       flex-direction: row;
     }
 
     .portfolio-item section {
-      flex-direction: row;
       padding-right: 1em;
     }
 
     .portfolio-item :global(h1) {
       font-size: 2.25em;
       text-align: left;
+    }
+
+    .portfolio-item :global(footer) {
+      flex: 1;
+      display: flex;
+      align-items: flex-end;
     }
   }
 </style>
