d4190c495fed88f4116945ecb9d6ebbaa5406d16
auroratide <tf.auroratide@gmail.com>
2019-11-12 06:37:53 -0600

    Include props in rendering

--- src/md-renderer/index.js
+++ src/md-renderer/index.js
@@ -2,11 +2,20 @@ import { Parser } from './parsing';
 import Footer from './custom/Footer';
 import Skills from './custom/Skills';
 
+const props = p => {
+  if(p)
+    return ' ' + Object.entries(p).map(([key, value]) =>
+      `${key === 'className' ? 'class' : key}="${value}"`
+    ).join(' ');
+  else
+    return '';
+};
+
 const render = value => {
   if(Array.isArray(value)) {
     return value.map(render).join('');
   } else if(value && value.c) {
-    return `<${value.c}>${render(value.d)}</${value.c}>`
+    return `<${value.c}${props(value.p)}>${render(value.d)}</${value.c}>`
   } else {
     return value;
   }
